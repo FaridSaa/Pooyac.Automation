112684a6544d78a25bc3fe4b9dc5ca047b367857
FaridSa <FaridSa@DESKTOP-AGVTG9C>
2024-10-18 23:18:23 +0330

    add packages

---
+++ Src/Automation.Application/Validation/CreateWipDtoValidation.cs
@@ -0,0 +1,13 @@
+﻿using Automation.Domain.Model.Incoming;
+using FluentValidation;
+
+namespace Automation.Application.Validation
+{
+    public class CreateWipDtoValidation :  AbstractValidator<CreateWipDto>
+    {
+        public CreateWipDtoValidation()
+        {
+            this.RuleFor(m => m.WorkItems).NotEmpty();
+        }
+    }
+}
